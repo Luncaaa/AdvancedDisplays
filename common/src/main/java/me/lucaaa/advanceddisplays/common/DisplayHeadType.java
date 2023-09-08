d21269cf5750d77f55329e635e3059c19f577953
Luncaaa <luca.alavoir@gmail.com>
2023-09-09 00:23:51 +0200

    [1.2.3] - Add a comment

--- common/src/main/java/me/lucaaa/advanceddisplays/common/DisplayHeadType.java
+++ common/src/main/java/me/lucaaa/advanceddisplays/common/DisplayHeadType.java
@@ -3,6 +3,7 @@ package me.lucaaa.advanceddisplays.common;
 public enum DisplayHeadType {
     NONE(null), PLAYER("player"), BASE64("base64");
 
+    // configName is the name that the setting will have in the config. Used to get a setting by the type of the displayed head.
     private final String configName;
 
     DisplayHeadType(String configName) {

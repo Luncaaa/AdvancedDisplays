eb6680f97815880211f00c0cb919e5c302bb8b8b
Luncaaa <luca.alavoir@gmail.com>
2023-10-04 23:49:19 +0200

    [1.3] - Refactor and add comments

--- plugin/src/main/java/me/lucaaa/advanceddisplays/AdvancedDisplays.java
+++ plugin/src/main/java/me/lucaaa/advanceddisplays/AdvancedDisplays.java
@@ -1,10 +1,12 @@
 package me.lucaaa.advanceddisplays;
 
+import me.lucaaa.advanceddisplays.api.DisplaysManager;
 import me.lucaaa.advanceddisplays.commands.MainCommand;
 import me.lucaaa.advanceddisplays.commands.subCommands.SubCommandsFormat;
+import me.lucaaa.advanceddisplays.common.managers.ConfigManager;
+import me.lucaaa.advanceddisplays.common.managers.PacketsManager;
 import me.lucaaa.advanceddisplays.events.PlayerEventsListener;
-import me.lucaaa.advanceddisplays.managers.*;
-import me.lucaaa.advanceddisplays.common.Logger;
+import me.lucaaa.advanceddisplays.common.utils.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,30 +16,10 @@ import java.util.HashMap;
 import java.util.Objects;
 import java.util.logging.Level;
 
-//todo - API implementation
-/*
-public void setText(String text) {
-    this.text = text;
-    if (this.config != null) {
-        this.settings.set("text", text);
-        this.save();
-    }
-
-    for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-        this.setText(text, onlinePlayer);
-    }
-}
-
-public void setText(String text, Player player) {
-        // just send packets
-        if (text.size() == 1) {
-            this.packets.setText(this.displayId, text.get(0), player);
-        } else {
-            this.textRunnable.stop();
-            this.textRunnable.start(text);
-        }
-}
- */
+// TODO
+// 1. Fix movehere method between worlds.
+// 2. Add more text methods (add, remove, set order, per player...)
+// 3. Animated item and block displays?
 
 //todo
 /*
@@ -77,11 +59,7 @@ public class AdvancedDisplays extends JavaPlugin {
     public static ConfigManager mainConfig;
 
     // Managers.
-    public static PacketsManager packetsManager;
     public static DisplaysManager displaysManager;
-
-    // Conversion
-    public static boolean needsConversion = false;
 
     // Reload the config files.
     public static void reloadConfigs() {
@@ -92,7 +70,7 @@ public class AdvancedDisplays extends JavaPlugin {
         mainConfig = new ConfigManager(plugin, "config.yml");
 
         // Managers
-        packetsManager = new PacketsManager(Bukkit.getServer().getClass().getName().split("\\.")[3]);
+        PacketsManager.setPackets(Bukkit.getServer().getClass().getName().split("\\.")[3]);
         if (displaysManager != null) displaysManager.removeAllEntities(); // If the plugin has been reloaded, remove the displays to prevent duplicate displays.
         displaysManager = new DisplaysManager(plugin, "displays");
     }

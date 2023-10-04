b63062970159427dccfcdd412194b7a41ad693b4
Luncaaa <luca.alavoir@gmail.com>
2023-10-04 23:00:42 +0200

    [1.3] - Improve the conversion system

--- plugin/src/main/java/me/lucaaa/advanceddisplays/commands/subCommands/ConvertSubCommand.java
+++ plugin/src/main/java/me/lucaaa/advanceddisplays/commands/subCommands/ConvertSubCommand.java
@@ -1,8 +1,9 @@
 package me.lucaaa.advanceddisplays.commands.subCommands;
 
 import me.lucaaa.advanceddisplays.AdvancedDisplays;
+import me.lucaaa.advanceddisplays.common.managers.ConversionManager;
 import me.lucaaa.advanceddisplays.managers.MessagesManager;
-import me.lucaaa.advanceddisplays.displays.DisplayType;
+import me.lucaaa.advanceddisplays.api.displays.enums.DisplayType;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
@@ -15,24 +16,25 @@ import java.util.*;
 
 public class ConvertSubCommand extends SubCommandsFormat {
     private boolean hasRunOnce = false;
+    private final ArrayList<String> validVersions = new ArrayList<>(Arrays.asList("1.0", "1.1", "1.2"));
 
     public ConvertSubCommand() {
         this.name = "convert";
         this.description = "Converts old display configurations to newer versions.";
         this.usage = "/ad convert [previous version]";
         this.minArguments = 1;
-        this.executableByConsole = false;
+        this.executableByConsole = true;
         this.neededPermission = "ad.convert";
     }
 
     @Override
     public ArrayList<String> getTabCompletions(CommandSender sender, String[] args) {
-        return new ArrayList<>(Arrays.asList("1.0", "1.1", "1.2"));
+        return validVersions;
     }
 
     @Override
     public void run(CommandSender sender, String[] args) {
-        if (!AdvancedDisplays.needsConversion) {
+        if (!ConversionManager.isConversionNeeded()) {
             sender.sendMessage(MessagesManager.getColoredMessage("&aConfiguration files seem to be up-to-date. If this is an error, please report it on GitHub.", true));
             return;
         }
@@ -44,132 +46,118 @@ public class ConvertSubCommand extends SubCommandsFormat {
             return;
         }
 
-        switch (args[1]) {
+        if (!validVersions.contains(args[1])) {
+            sender.sendMessage(MessagesManager.getColoredMessage("&b" + args[1] + " &cis not a valid version.", true));
+        }
+
+        this.loopFiles(args[1], new File(AdvancedDisplays.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "displays"));
+
+        ConversionManager.setConversionNeeded(false);
+        AdvancedDisplays.reloadConfigs();
+        sender.sendMessage(MessagesManager.getColoredMessage("&aThe displays have been successfully converted!", true));
+    }
+
+    private void loopFiles(String previousVersion, File fileOrDir) {
+        if (fileOrDir.isDirectory()) {
+            for (File insideFileOrDir : Objects.requireNonNull(fileOrDir.listFiles())) {
+                this.loopFiles(previousVersion, insideFileOrDir);
+            }
+
+        } else {
+            this.convert(previousVersion, fileOrDir);
+        }
+    }
+
+    private void convert(String previousVersion, File file) {
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        ConfigurationSection settingsSection;
+        if (config.contains("settings")) {
+            settingsSection = config.getConfigurationSection("settings");
+        } else {
+            settingsSection = config.createSection("settings");
+        }
+        assert settingsSection != null;
+
+        switch (previousVersion) {
             case "1.0" -> {
-                for (File configFile : Objects.requireNonNull(new File(AdvancedDisplays.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "displays").listFiles())) {
-                    YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
-                    ConfigurationSection settingsSection = config.createSection("settings");
+                if (config.getString("block") != null) {
+                    config.set("type", DisplayType.BLOCK.name());
+                    settingsSection.set("block", config.getString("block"));
+                    config.set("block", null);
 
-                    if (config.getString("block") != null) {
-                        config.set("type", DisplayType.BLOCK.name());
-                        settingsSection.set("block", config.getString("block"));
-                        config.set("block", null);
+                } else if (config.getString("item") != null) {
+                    config.set("type", DisplayType.ITEM.name());
+                    settingsSection.set("item", config.getString("item"));
+                    settingsSection.set("enchanted", false);
+                    settingsSection.set("itemTransformation", config.getString("itemTransformation"));
+                    config.set("item", null);
+                    config.set("itemTransformation", null);
 
-                    } else if (config.getString("item") != null) {
-                        config.set("type", DisplayType.ITEM.name());
-                        settingsSection.set("item", config.getString("item"));
-                        settingsSection.set("enchanted", false);
-                        settingsSection.set("itemTransformation", config.getString("itemTransformation"));
-                        config.set("item", null);
-                        config.set("itemTransformation", null);
-
-                    } else if (config.getString("text") != null) {
-                        config.set("type", DisplayType.TEXT.name());
-                        settingsSection.set("animationTime", 20);
-                        settingsSection.set("refreshTime", 20);
-                        settingsSection.set("text", List.of(config.getString("text")));
-                        settingsSection.set("alignment", config.getString("alignment"));
-                        settingsSection.set("backgroundColor", config.getString("backgroundColor") + ";255");
-                        settingsSection.set("lineWidth", config.getInt("lineWidth"));
-                        settingsSection.set("textOpacity", config.getInt("textOpacity"));
-                        settingsSection.set("defaultBackground", config.getBoolean("defaultBackground"));
-                        settingsSection.set("seeThrough", config.getBoolean("seeThrough"));
-                        config.set("text", null);
-                        config.set("alignment", null);
-                        config.set("backgroundColor", null);
-                        config.set("lineWidth", null);
-                        config.set("textOpacity", null);
-                        config.set("defaultBackground", null);
-                        config.set("seeThrough", null);
-                    }
-
-                    Display display = (Display) Bukkit.getEntity(UUID.fromString(Objects.requireNonNull(config.getString("id"))));
-                    display.remove();
-                    config.set("id", null);
-
-                    try {
-                        AdvancedDisplays.mainConfig.getConfig().set("text-update", 20);
-                        AdvancedDisplays.mainConfig.save();
-                        config.save(configFile);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
+                } else if (config.getString("text") != null) {
+                    config.set("type", DisplayType.TEXT.name());
+                    settingsSection.set("animationTime", 20);
+                    settingsSection.set("refreshTime", 20);
+                    settingsSection.set("text", List.of(Objects.requireNonNull(config.getString("text"))));
+                    settingsSection.set("alignment", config.getString("alignment"));
+                    settingsSection.set("backgroundColor", config.getString("backgroundColor") + ";255");
+                    settingsSection.set("lineWidth", config.getInt("lineWidth"));
+                    settingsSection.set("textOpacity", config.getInt("textOpacity"));
+                    settingsSection.set("defaultBackground", config.getBoolean("defaultBackground"));
+                    settingsSection.set("seeThrough", config.getBoolean("seeThrough"));
+                    config.set("text", null);
+                    config.set("alignment", null);
+                    config.set("backgroundColor", null);
+                    config.set("lineWidth", null);
+                    config.set("textOpacity", null);
+                    config.set("defaultBackground", null);
+                    config.set("seeThrough", null);
                 }
+
+                Display display = (Display) Bukkit.getEntity(UUID.fromString(Objects.requireNonNull(config.getString("id"))));
+                Objects.requireNonNull(display).remove();
+                config.set("id", null);
             }
 
             case "1.1" -> {
-                for (File configFile : Objects.requireNonNull(new File(AdvancedDisplays.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "displays").listFiles())) {
-                    YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
-                    DisplayType type = DisplayType.valueOf(config.getString("type"));
-                    if (type == DisplayType.BLOCK) continue;
+                DisplayType type = DisplayType.valueOf(config.getString("type"));
+                if (type == DisplayType.BLOCK) break;
 
-                    ConfigurationSection settingsSection = config.getConfigurationSection("settings");
+                if (type == DisplayType.TEXT) {
+                    settingsSection.set("animationTime", 20);
+                    settingsSection.set("refreshTime", 20);
+                    settingsSection.set("text", List.of(Objects.requireNonNull(settingsSection.get("text"))));
 
-                    if (type == DisplayType.TEXT) {
-                        settingsSection.set("animationTime", 20);
-                        settingsSection.set("refreshTime", 20);
-                        settingsSection.set("text", List.of(settingsSection.get("text")));
-
-                    } else if (type == DisplayType.ITEM) {
-                        settingsSection.set("enchanted", false);
-                    }
-
-                    try {
-                        config.save(configFile);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
+                } else if (type == DisplayType.ITEM) {
+                    settingsSection.set("enchanted", false);
                 }
             }
 
             case "1.2" -> {
-                for (File configFile : Objects.requireNonNull(new File(AdvancedDisplays.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "displays").listFiles())) {
-                    YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
-                    DisplayType type = DisplayType.valueOf(config.getString("type"));
-                    if (type == DisplayType.BLOCK) continue;
+                DisplayType type = DisplayType.valueOf(config.getString("type"));
+                if (type == DisplayType.BLOCK) break;
 
-                    ConfigurationSection settingsSection = config.getConfigurationSection("settings");
+                if (type == DisplayType.TEXT) {
+                    settingsSection.set("animationTime", 20);
+                    settingsSection.set("refreshTime", 20);
 
-                    if (type == DisplayType.TEXT) {
-                        settingsSection.set("animationTime", 20);
-                        settingsSection.set("refreshTime", 20);
-
-                    } else if (type == DisplayType.ITEM) {
-                        settingsSection.set("enchanted", false);
-                    }
-
-                    try {
-                        AdvancedDisplays.mainConfig.getConfig().set("text-update", null);
-                        AdvancedDisplays.mainConfig.save();
-                        config.save(configFile);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
+                } else if (type == DisplayType.ITEM) {
+                    settingsSection.set("enchanted", false);
                 }
+
+                AdvancedDisplays.mainConfig.getConfig().set("text-update", null);
+                AdvancedDisplays.mainConfig.save();
             }
 
             case "1.2.1" -> {
-                for (File configFile : Objects.requireNonNull(new File(AdvancedDisplays.getPlugin().getDataFolder().getAbsolutePath() + File.separator + "displays").listFiles())) {
-                    YamlConfiguration config = YamlConfiguration.loadConfiguration(configFile);
-                    if (DisplayType.valueOf(config.getString("type")) != DisplayType.ITEM) continue;
-
-                    ConfigurationSection settingsSection = config.getConfigurationSection("settings");
-                    settingsSection.set("enchanted", false);
-
-                    try {
-                        config.save(configFile);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
+                if (DisplayType.valueOf(config.getString("type")) != DisplayType.ITEM) break;
+                settingsSection.set("enchanted", false);
             }
-
-            default -> sender.sendMessage(MessagesManager.getColoredMessage("&b" + args[1] + " &cis not a valid version.", true));
         }
 
-        AdvancedDisplays.needsConversion = false;
-        this.hasRunOnce = false;
-        AdvancedDisplays.reloadConfigs();
-        sender.sendMessage(MessagesManager.getColoredMessage("&aThe displays have been successfully converted!", true));
+        try {
+            config.save(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }

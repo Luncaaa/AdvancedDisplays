00c3e6a70f2841e1a69164aa446a64972399c12b
Luncaaa <luca.alavoir@gmail.com>
2024-01-06 00:21:17 +0100

    [1.3] - Add the ability to modify a block's data from the config file

--- plugin/src/main/java/me/lucaaa/advanceddisplays/displays/ADBlockDisplay.java
+++ plugin/src/main/java/me/lucaaa/advanceddisplays/displays/ADBlockDisplay.java
@@ -3,12 +3,12 @@ package me.lucaaa.advanceddisplays.displays;
 import me.lucaaa.advanceddisplays.api.displays.enums.DisplayType;
 import me.lucaaa.advanceddisplays.common.managers.ConfigManager;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.BlockDisplay;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class ADBlockDisplay extends ADBaseDisplay implements DisplayMethods, me.lucaaa.advanceddisplays.api.displays.BlockDisplay {
@@ -20,7 +20,14 @@ public class ADBlockDisplay extends ADBaseDisplay implements DisplayMethods, me.
         this.settings = this.config.getConfigurationSection("settings");
 
         if (this.settings != null) {
-            this.block = Objects.requireNonNull(Material.getMaterial(Objects.requireNonNull(this.settings.getString("block")))).createBlockData();
+            String blockData = "minecraft:" + Objects.requireNonNull(this.settings.getString("block")).toLowerCase() + "[";
+            ConfigurationSection dataSection = Objects.requireNonNull(this.settings.getConfigurationSection("blockData"));
+            ArrayList<String> dataParts = new ArrayList<>();
+            for (String dataKey : dataSection.getKeys(false)) {
+                dataParts.add(dataKey + "=" + dataSection.get(dataKey));
+            }
+            blockData = blockData.concat(String.join(",", dataParts));
+            this.block = Bukkit.getServer().createBlockData(blockData + "]");
         }
     }
     public ADBlockDisplay(BlockDisplay display) {
@@ -46,8 +53,17 @@ public class ADBlockDisplay extends ADBaseDisplay implements DisplayMethods, me.
     @Override
     public void setBlock(BlockData block) {
         this.block = block;
+
         if (this.config != null) {
             this.settings.set("block", block.getMaterial().name());
+
+            String fullData = block.getAsString().substring(block.getAsString().indexOf("[") + 1, block.getAsString().lastIndexOf("]"));
+            ConfigurationSection dataSection = this.settings.createSection("blockData");
+            for (String data : fullData.split(",")) {
+                String[] dataPart = data.split("=");
+                dataSection.set(dataPart[0], dataPart[1]);
+            }
+
             this.save();
         }
         for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {

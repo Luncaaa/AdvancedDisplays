8c8c79a929b855a2605162ef775aae9ab9884bbc
Luncaaa <luca.alavoir@gmail.com>
2024-01-06 13:30:45 +0100

    [1.3] - Remove dependency and modify Javadocs

--- api/src/main/java/me/lucaaa/advanceddisplays/api/ADAPIProvider.java
+++ api/src/main/java/me/lucaaa/advanceddisplays/api/ADAPIProvider.java
@@ -1,17 +1,14 @@
 package me.lucaaa.advanceddisplays.api;
 
 import org.bukkit.plugin.Plugin;
-import org.jetbrains.annotations.ApiStatus;
 
 /**
  * INTERNAL USE ONLY - DO NOT USE!
  */
-@ApiStatus.Internal
 public abstract class ADAPIProvider {
 
     private static ADAPIProvider implementation;
 
-    @ApiStatus.Internal
     public static ADAPIProvider getImplementation() {
         if (ADAPIProvider.implementation == null) {
             throw new IllegalStateException("The AdvancedDisplays API implementation is not set yet.");
@@ -19,7 +16,6 @@ public abstract class ADAPIProvider {
         return ADAPIProvider.implementation;
     }
 
-    @ApiStatus.Internal
     public static void setImplementation(ADAPIProvider implementation) {
         if (ADAPIProvider.implementation != null) {
             throw new IllegalStateException("The AdvancedDisplays API implementation is already set.");

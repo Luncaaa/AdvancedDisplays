23de70177b105f0f95f7545ffe413b49a32df036
Luncaaa <luca.alavoir@gmail.com>
2024-07-14 01:55:24 +0200

    [1.5.1] - Fix right-clicking when editing subtracting double

--- plugin/src/main/java/me/lucaaa/advanceddisplays/events/PlayerEventsListener.java
+++ plugin/src/main/java/me/lucaaa/advanceddisplays/events/PlayerEventsListener.java
@@ -8,16 +8,19 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 @SuppressWarnings("unused")
 public class PlayerEventsListener implements Listener {
     private final AdvancedDisplays plugin;
+    private final HashMap<Player, Long> pastInteractions = new HashMap<>();
 
     public PlayerEventsListener(AdvancedDisplays plugin) {
         this.plugin = plugin;
@@ -78,6 +81,22 @@ public class PlayerEventsListener implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         if (plugin.getInventoryManager().isPlayerNotEditing(event.getPlayer()) || event.getHand() == EquipmentSlot.OFF_HAND) return;
+
+        Player player = event.getPlayer();
+        Action action = event.getAction();
+
+        // Because the event is fired twice, the current time is stored in a map along with the player that interacted with the display.
+        // When the event is called again, the current time and the one stored in the map are compared. If less than or 20ms have passed, ignore this event.
+        if (action == Action.RIGHT_CLICK_AIR) {
+            if (this.pastInteractions.containsKey(player)) {
+                long now = System.currentTimeMillis();
+                if (now - this.pastInteractions.remove(player) <= 500) {
+                    return;
+                }
+            } else {
+                this.pastInteractions.put(player, System.currentTimeMillis());
+            }
+        }
 
         event.setCancelled(true);
         plugin.getInventoryManager().getEditingPlayer(event.getPlayer()).handleClick(event);

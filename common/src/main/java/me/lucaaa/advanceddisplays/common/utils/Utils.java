20115bf7cfdc6f5632b97c6f8e9c69a5b2e07c17
Luncaaa <luca.alavoir@gmail.com>
2024-07-27 06:06:15 +0200

    [1.5.1] - Improve color parsing

--- common/src/main/java/me/lucaaa/advanceddisplays/common/utils/Utils.java
+++ common/src/main/java/me/lucaaa/advanceddisplays/common/utils/Utils.java
@@ -3,7 +3,8 @@ package me.lucaaa.advanceddisplays.common.utils;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
+import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
 import org.bukkit.Bukkit;
@@ -16,26 +17,29 @@ public class Utils {
     }
 
     public static String getColoredTextWithPlaceholders(Player player, String text) {
-        String transformedText = text.replace("%player%", player.getName());
-
-        if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            transformedText = PlaceholderAPI.setPlaceholders(player, transformedText);
-        }
-
-        Component c = MiniMessage.miniMessage().deserialize(transformedText);
-        return ChatColor.translateAlternateColorCodes('&', JSONComponentSerializer.json().serialize(c)).replace("\\n", "\n");
+        return ComponentSerializer.toString(parseMessage(player, null, text, false));
     }
 
-    public static BaseComponent[] getTextComponent(String message, Player clickedPlayer, Player actionPlayer, boolean useGlobalPlaceholders) {
-        String transformedText = message.replace("%player%", clickedPlayer.getName()).replace("%global_player%", actionPlayer.getName());
+    public static BaseComponent[] getTextComponent(String message, Player clickedPlayer, Player globalPlayer, boolean useGlobalPlaceholders) {
+        return parseMessage(clickedPlayer, globalPlayer, message, useGlobalPlaceholders);
+    }
 
-        Player placeholderPlayer = (useGlobalPlaceholders) ? actionPlayer : clickedPlayer;
+    private static BaseComponent[] parseMessage(Player player, Player globalPlayer, String message, boolean useGlobalPlaceholders) {
+        message = message.replace("%player%", player.getName());
+        if (globalPlayer != null) message = message.replace("%global_player%", globalPlayer.getName());
+
+        Player placeholderPlayer = (useGlobalPlaceholders) ? globalPlayer : player;
         if (Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            transformedText = PlaceholderAPI.setPlaceholders(placeholderPlayer, transformedText);
+            message = PlaceholderAPI.setPlaceholders(placeholderPlayer, message);
         }
 
-        Component c = MiniMessage.miniMessage().deserialize(transformedText);
-        String json = ChatColor.translateAlternateColorCodes('&', JSONComponentSerializer.json().serialize(c)).replace("\\n", "\n");
-        return ComponentSerializer.parse(json);
+        // From legacy and minimessage format to a component
+        Component legacy = LegacyComponentSerializer.legacyAmpersand().deserialize(message);
+        // From component to Minimessage String. Replacing the "\" with nothing makes the minimessage formats work.
+        String minimessage = MiniMessage.miniMessage().serialize(legacy).replace("\\", "");
+        // From Minimessage String to Minimessage component
+        Component component = MiniMessage.miniMessage().deserialize(minimessage);
+        // From Minimessage component to legacy string.
+        return BungeeComponentSerializer.get().serialize(component);
     }
 }

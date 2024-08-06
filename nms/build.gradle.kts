6c5ad46ecdb936ae4908daf9af07eb5fd451cde0
Luncaaa <luca.alavoir@gmail.com>
2024-08-06 21:42:31 +0200

    [1.5.2] - Reduce JAR size

--- nms/build.gradle.kts
+++ nms/build.gradle.kts
@@ -1,6 +1,8 @@
 subprojects {
     dependencies {
-        implementation(project(":common"))
-        implementation(project(":api"))
+        // So that minimizing does not include adventure.
+        // After all, these dependencies will already be shaded in the final JAR.
+        compileOnly(project(":common"))
+        compileOnly(project(":api"))
     }
 }

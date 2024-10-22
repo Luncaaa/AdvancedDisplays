df92a92364d0a46c641c958f6adbc1ffd1f99263
Luncaaa <luca.alavoir@gmail.com>
2024-10-23 00:16:24 +0200

    [1.5.6] - Add 1.21.2 support

--- plugin/src/main/java/me/lucaaa/advanceddisplays/data/Version.java
+++ plugin/src/main/java/me/lucaaa/advanceddisplays/data/Version.java
@@ -9,7 +9,7 @@ public enum Version {
     v1_20_R2(List.of("1.20.2")),
     v1_20_R3(List.of("1.20.3", "1.20.4")),
     v1_20_R4(List.of("1.20.5", "1.20.6")),
-    v1_21_R1(List.of("1.21", "1.21.1"));
+    v1_21_R1(List.of("1.21", "1.21.1", "1.21.2"));
 
     private final List<String> mcVersions;
     Version(List<String> mcVersions) {

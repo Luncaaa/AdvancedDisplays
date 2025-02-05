21088a9ca83d401f2f5295cf4d0e803f6cbc031e
Luncaaa <luca.alavoir@gmail.com>
2025-02-05 16:30:31 +0100

    [1.5.9] - Update versions

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -20,7 +20,7 @@ tasks {
             (this as StandardJavadocDocletOptions).apply {
                 // ©
                 bottom = "Copyright 2024 Lucaaa. All rights reserved. Licensed under GPL 3.0. View the source code <a href=\"https://github.com/Luncaaa/AdvancedDisplays\">here</a>"
-                links = listOf("https://hub.spigotmc.org/javadocs/spigot", "https://jd.advntr.dev/api/4.17.0/")
+                links = listOf("https://hub.spigotmc.org/javadocs/spigot", "https://jd.advntr.dev/api/4.18.0/")
                 header = "<div style=\"font-size: 25px\"><a href=\"https://github.com/Luncaaa\">By Lucaaa</a>    |    <a href=\"https://spigotmc.org/resources/authors/lucaaa.1192446/\">More plugins</a></div>"
             }
         }
@@ -36,7 +36,7 @@ publishing {
         val mavenJava by creating(MavenPublication::class) {
             groupId = "AdvancedDisplays"
             artifactId = "advanceddisplays-api"
-            version = "1.5.8"
+            version = "${project.version}"
 
             from(components["java"])
         }

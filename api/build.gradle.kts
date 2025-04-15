967de89b8f25af6187edb2436f6d4c92a238e5bc
Luncaaa <luca.alavoir@gmail.com>
2025-04-15 16:51:21 +0200

    [1.6] - Update Javadoc

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -11,8 +11,10 @@ tasks {
         title = "AdvancedDisplays API " + project.version
         options {
             (this as StandardJavadocDocletOptions).apply {
-                // ©
-                bottom = "Copyright 2024 Lucaaa. All rights reserved. Licensed under GPL 3.0. View the source code <a href=\"https://github.com/Luncaaa/AdvancedDisplays\">here</a>"
+                charSet = "UTF-8"
+                encoding = "UTF-8"
+                docEncoding = "UTF-8"
+                bottom = "Copyright © 2025 Lucaaa. All rights reserved. Licensed under GPL 3.0. View the source code <a href=\"https://github.com/Luncaaa/AdvancedDisplays\">here</a>"
                 links = listOf("https://hub.spigotmc.org/javadocs/spigot", "https://jd.advntr.dev/api/4.18.0/")
                 header = "<div style=\"font-size: 25px\"><a href=\"https://github.com/Luncaaa\">By Lucaaa</a>    |    <a href=\"https://spigotmc.org/resources/authors/lucaaa.1192446/\">More plugins</a></div>"
             }

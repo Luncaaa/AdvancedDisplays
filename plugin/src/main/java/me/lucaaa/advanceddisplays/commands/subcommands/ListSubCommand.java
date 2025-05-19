2931479b2e2ff095486b7a7d3cce303504b9fee4
Luncaaa <luca.alavoir@gmail.com>
2025-05-19 03:28:22 +0200

    [1.7] - Show message for no displays

--- plugin/src/main/java/me/lucaaa/advanceddisplays/commands/subcommands/ListSubCommand.java
+++ plugin/src/main/java/me/lucaaa/advanceddisplays/commands/subcommands/ListSubCommand.java
@@ -3,6 +3,8 @@ package me.lucaaa.advanceddisplays.commands.subcommands;
 import me.lucaaa.advanceddisplays.AdvancedDisplays;
 import org.bukkit.command.CommandSender;
 
+import java.util.Set;
+
 public class ListSubCommand extends SubCommandsFormat{
     public ListSubCommand(AdvancedDisplays plugin) {
         super(plugin);
@@ -16,7 +18,13 @@ public class ListSubCommand extends SubCommandsFormat{
 
     @Override
     public void run(CommandSender sender, String[] args) {
-        String list = String.join("&6,&e ", plugin.getDisplaysManager().getDisplays().keySet());
+        Set<String> keys = plugin.getDisplaysManager().getDisplays().keySet();
+        String list;
+        if (keys.isEmpty()) {
+            list = "&7No displays found.";
+        } else {
+            list = String.join("&6,&e ", plugin.getDisplaysManager().getDisplays().keySet());
+        }
         sender.sendMessage(plugin.getMessagesManager().getColoredMessage("&6List of displays: &e" + list));
     }
 }

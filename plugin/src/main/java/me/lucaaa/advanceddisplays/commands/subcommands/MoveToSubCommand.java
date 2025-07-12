ef3604955204b8fa6436d7fd5ab71ed352a37b2d
Luncaaa <luca.alavoir@gmail.com>
2025-07-12 23:19:18 +0200

    [1.7] - Create moveto subcommand

---
+++ plugin/src/main/java/me/lucaaa/advanceddisplays/commands/subcommands/MoveToSubCommand.java
@@ -0,0 +1,107 @@
+package me.lucaaa.advanceddisplays.commands.subcommands;
+
+import me.lucaaa.advanceddisplays.AdvancedDisplays;
+import me.lucaaa.advanceddisplays.api.displays.BaseEntity;
+import me.lucaaa.advanceddisplays.data.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class MoveToSubCommand extends SubCommandsFormat {
+    public MoveToSubCommand(AdvancedDisplays plugin) {
+        super(plugin);
+        this.name = "moveto";
+        this.description = "Moves a display to the given location.";
+        this.usage = "/ad moveto [name] [x/~/*] [y/~/*] [z/~/*] <world/~/*>";
+        this.minArguments = 4;
+        this.executableByConsole = true;
+        this.neededPermission = "ad.moveto";
+    }
+
+    @Override
+    public List<String> getTabCompletions(CommandSender sender, String[] args) {
+        return switch (args.length) {
+            case 2 -> plugin.getDisplaysManager().getDisplays().keySet().stream().toList();
+            case 3, 4, 5 -> List.of("~", "*");
+            case 6 -> new java.util.ArrayList<>(plugin.getServer().getWorlds().stream().map(World::getName).toList());
+            default -> List.of();
+        };
+    }
+
+    @Override
+    public void run(CommandSender sender, String[] args) {
+        BaseEntity display = plugin.getDisplaysManager().getDisplays().get(args[1]);
+
+        if (display == null) {
+            sender.sendMessage(plugin.getMessagesManager().getColoredMessage("&cThe display &b" + args[1] + " &cdoes not exist!"));
+            return;
+        }
+
+        if (String.join(" ", args).contains("~") && !(sender instanceof Player)) {
+            sender.sendMessage(plugin.getMessagesManager().getColoredMessage("&cOnly players can use \"~\" (relative to the player's position)."));
+            return;
+        }
+
+        World world;
+        if (args.length >= 6) {
+            String worldName = String.join(" ", Arrays.copyOfRange(args, 5, args.length));
+            world = Bukkit.getWorld(worldName);
+
+            if (world == null) {
+                sender.sendMessage(plugin.getMessagesManager().getColoredMessage("&cThe world &b" + worldName + " &cdoes not exist!"));
+                return;
+            }
+        } else {
+            world = display.getLocation().getWorld();
+        }
+
+        try {
+            double x = getNewPosition(args[2], CoordComponent.X, display, sender);
+            double y = getNewPosition(args[3], CoordComponent.Y, display, sender);
+            double z = getNewPosition(args[4], CoordComponent.Z, display, sender);
+
+            display.setLocation(new Location(world, x, y, z));
+            sender.sendMessage(plugin.getMessagesManager().getColoredMessage("&aThe display has been successfully moved to &e" + x + " " + y + " " + z + " &ain world &e" + Objects.requireNonNull(world).getName()));
+        } catch (NumberFormatException e) {
+            sender.sendMessage(plugin.getMessagesManager().getColoredMessage("&cOne of the coordinates is not a valid number or relative position! For decimals use dots (\".\"), NOT commas (\",\")."));
+        }
+
+    }
+
+    private Double getNewPosition(String toParse, CoordComponent component, BaseEntity display, CommandSender sender) throws NumberFormatException {
+        double parsedArg = 0;
+
+        if (toParse.contains("~")) {
+            Player player = (Player) sender;
+            toParse = toParse.replace("~", "");
+            parsedArg = switch (component) {
+                case X -> player.getLocation().getX();
+                case Y -> player.getLocation().getY() ;
+                case Z -> player.getLocation().getZ();
+            };
+
+        } else if (toParse.contains("*")) {
+            toParse = toParse.replace("*", "");
+            parsedArg = switch (component) {
+                case X -> display.getLocation().getX();
+                case Y -> display.getLocation().getY();
+                case Z -> display.getLocation().getZ();
+            };
+        }
+
+        double parsed = toParse.isBlank() ? 0 : Double.parseDouble(toParse);
+        return Utils.round(parsedArg + parsed);
+    }
+
+    private enum CoordComponent {
+        X,
+        Y,
+        Z
+    }
+}

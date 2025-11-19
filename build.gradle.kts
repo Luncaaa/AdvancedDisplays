c89d7219ec61d9dadbdf63f7f06589b137e1a3c2
Luncaaa <luca.alavoir@gmail.com>
2025-11-19 15:38:08 +0100

    [1.7] - Update dependencies and fix broken builds

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,17 +24,18 @@ subprojects {
     repositories {
         mavenCentral()
         mavenLocal()
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     }
 
     dependencies {
         compileOnly("org.spigotmc:spigot-api:1.19.4-R0.1-SNAPSHOT")
-        compileOnly("io.netty:netty-all:4.2.3.Final")
-        implementation("net.kyori:adventure-api:4.24.0")
-        implementation("net.kyori:adventure-text-minimessage:4.24.0")
-        implementation("net.kyori:adventure-text-serializer-legacy:4.24.0")
-        implementation("net.kyori:adventure-text-serializer-gson:4.24.0")
+        compileOnly("io.netty:netty-all:4.2.7.Final")
+        implementation("net.kyori:adventure-api:4.25.0")
+        implementation("net.kyori:adventure-text-minimessage:4.25.0")
+        implementation("net.kyori:adventure-text-serializer-legacy:4.25.0")
+        implementation("net.kyori:adventure-text-serializer-gson:4.25.0")
         implementation("net.kyori:adventure-platform-bukkit:4.4.1")
     }
 }
